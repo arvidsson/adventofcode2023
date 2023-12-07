4d64f56ade301bed54353e24233087d792111253
arvidsson <arvidsson@gmail.com>
2023-12-07 19:53:35 +0100

    simplified day2

--- day2.py
+++ day2.py
@@ -1,31 +1,16 @@
 import re
 
-red = 12
-green = 13
-blue = 14
+red, green, blue = 12, 13, 14
 
 with open("day2.txt") as f:
     value = 0
     for line in f:
-        valid = True
-        game = re.split(r":", line)
-        index = int(game[0].split()[1])
-        result = re.split(r";", game[1])
-        for part in result:
-            balls = re.split(r",", part)
-            for ball in balls:
-                ball = ball.split()
-                num = int(ball[0])
-                btype = ball[1]
-                if btype == "red":
-                    if num > red:
-                        valid = False
-                elif btype == "green":
-                    if num > green:
-                        valid = False
-                elif btype == "blue":
-                    if num > blue:
-                        valid = False
-        if valid is True:
-            value += index
+        game, *turns = re.split(r":|;", line.strip())
+        result = [
+            int(num) <= globals()[ball]
+            for turn in turns
+            for num, ball in (pair.split() for pair in re.split(r",", turn.strip()))
+        ]
+        if all(result):
+            value += int(game.split()[1])
     print(value)
