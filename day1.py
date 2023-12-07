c7706ca52cd44c4bb45538393005e9af7130a888
arvidsson <arvidsson@gmail.com>
2023-12-07 17:55:07 +0100

    simplified day1

--- day1.py
+++ day1.py
@@ -1,16 +1,11 @@
+import re
+
 with open("day1.txt") as f:
     value = 0
     for line in f:
-        first = "0"
-        last = "0"
-        for c in line:
-            if c.isdigit():
-                if first == "0":
-                    first = c
-                else:
-                    last = c
-        if last == "0":
-            last = first
+        digits = re.findall("\d", line)
+        first = digits[0]
+        last = digits[len(digits) - 1]
         num = int(first + last)
         value += num
     print(value)
